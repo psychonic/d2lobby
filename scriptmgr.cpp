e11c0999c7daaab120507201988fb158a1de4672
Nicholas Hastings <nshastings@gmail.com>
2016-05-17 06:14:55 -0400

    Add license headers.

--- scriptmgr.cpp
+++ scriptmgr.cpp
@@ -1,3 +1,23 @@
+/**
+ * D2Lobby - A Metamod:Source plugin for Dota 2 matches on dedicated servers.
+ * Copyright (C) 2015  Nicholas Hastings <nshastings@gmail.com>
+ * 
+ * This file is part of D2Lobby.
+ * 
+ * D2Lobby is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * D2Lobby is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "scriptmgr.h"
 
 #include "d2lobby.h"
